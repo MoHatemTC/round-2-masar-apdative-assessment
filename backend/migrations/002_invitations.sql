9af7e11d7f23a2b97a9cc9f7275a8f3942b0634d
MalakEhabb <141783527+MalakEhabb@users.noreply.github.com>
2026-07-19 22:53:11 +0300

    Update 002_invitations.sql

--- backend/migrations/002_invitations.sql
+++ backend/migrations/002_invitations.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS invitations (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    assessment_id UUID REFERENCES assessments(id),
+    assessment_id uuid references assessments(id) on delete cascade,
     candidate_email TEXT NOT NULL,
     status TEXT DEFAULT 'not-taken' CHECK (status IN ('not-taken', 'taken')),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
