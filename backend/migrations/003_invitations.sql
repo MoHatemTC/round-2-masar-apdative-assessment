77f4f980ddf87a15c8b495af1744995464285934
MalakEhabb <141783527+MalakEhabb@users.noreply.github.com>
2026-07-19 22:56:34 +0300

    Update 003_invitations.sql

--- backend/migrations/003_invitations.sql
+++ backend/migrations/003_invitations.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS invitations (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id uuid primary key default gen_random_uuid(),
     assessment_id uuid references assessments(id) on delete cascade,
     candidate_email TEXT NOT NULL,
     status TEXT DEFAULT 'not-taken' CHECK (status IN ('not-taken', 'taken')),
